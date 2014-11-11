c4cc9fa0e6dff33f5407305346ec499895955e81
Wojciech Polak <wojciechpolak@gmail.com>
2014-11-11 11:11:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,60 @@
+VC The Longest Streak
+=====================
+
+The `vcstreak.py` finds the longest streak of consecutive commit dates.
+It supports local Git, Mercurial, and Subversion repositories.
+
+	Usage: vcstreak.py [options] REPO-PATH
+
+	Options:
+	-h, --help            show this help message and exit
+	-r, --reverse         Reverse output results
+	-t NUM, --top=NUM     Show NUM top users (0 is unlimited and default)
+	-s NUM, --streaks=NUM Show NUM longest streaks
+	-e, --exclude-weekends
+	                      Exclude weekends
+	-n, --name            Alias for --id-author=name
+	--id-author=ID        Author ID strategy [name, email]. email is default
+	--author=PATTERN      Limit the commits output to ones with author header
+	--since=DATE          Show commits more recent than a specific date
+	--branch=NAME         Branch name (all branches by default)
+	--sortby=NAME         Sort by [streaks, commits] (streaks is default)
+	--output=FORMAT       Output print format [simple, json, xml, yaml]
+
+Author
+------
+
+* Wojciech Polak (http://wojciechpolak.org/)
+
+Example Usage
+-------------
+
+	$ vcstreak.py ~/src/repo/
+		1) Wojciech Polak (2065 commits)
+			53 days: 2011-11-29 - 2012-01-20 (89da9804fb..a1fdd91e35)
+			28 days: 2012-08-08 - 2012-09-04 (66a1bbe0f3..dbce082f1c)
+			27 days: 2011-08-26 - 2011-09-21 (3232fe6a43..8025cbc197)
+			26 days: 2011-05-03 - 2011-05-28 (c509a7cab7..6378b723a7)
+			24 days: 2012-02-09 - 2012-03-03 (6ec70fa2d1..47d7bd0f0d)
+		...
+
+	$ vcstreak.py --name --top 3 ~/src/git/
+		1) Junio C Hamano (15962 commits)
+			83 days: 2006-12-16 - 2007-03-08 (1576f5f7b2..bd1fc628b8)
+			54 days: 2005-09-30 - 2005-11-22 (6dc88cc0dc..302ebfe521)
+			48 days: 2006-03-24 - 2006-05-10 (84a9b58c42..618faa1dc7)
+			37 days: 2005-08-23 - 2005-09-28 (f5e375c9a9..bf7960eb51)
+			33 days: 2006-02-09 - 2006-03-13 (5f73076c1a..ea75cb7284)
+		2) Linus Torvalds (1109 commits)
+			30 days: 2005-04-07 - 2005-05-06 (e83c516331..ee267527aa)
+			14 days: 2005-06-17 - 2005-06-30 (c538d2d34a..d0efc8a71d)
+			12 days: 2005-05-29 - 2005-06-09 (b97e3dfa76..aa16021efc)
+			11 days: 2005-05-17 - 2005-05-27 (c9049d41b8..84c1afd7e7)
+			 8 days: 2005-07-03 - 2005-07-10 (8a65ff7666..cf219196a8)
+		3) Shawn O. Pearce (1334 commits)
+			12 days: 2007-01-20 - 2007-01-31 (81c0f29a56..76f8a302c7)
+			11 days: 2007-09-17 - 2007-09-27 (3849bfba84..0b2bc460fc)
+			 8 days: 2006-11-21 - 2006-11-28 (f18e40a1a6..67ffa11425)
+			 6 days: 2006-11-04 - 2006-11-09 (dfb960920d..2d19516db4)
+			 6 days: 2006-11-11 - 2006-11-16 (49b86f010c..c1237ae288)
+	2014-11-11T09:30:50Z
---
+++ vcstreak.py
@@ -0,0 +1,328 @@
+#!/usr/bin/env python
+
+# vcstreak.py -- Copyright (C) 2014 Wojciech Polak.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 3 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import datetime
+import subprocess
+from email.utils import mktime_tz, parsedate_tz, formatdate
+
+
+def calculate(options=None, args=None):
+    options = options or {}
+    args = args or ['.']
+    cwd = args[0]
+
+    commit_fields = [
+        'id',
+        'author_name',
+        'author_email',
+        'date',
+    ]
+
+    vcs = False
+    if os.path.isdir(os.path.join(cwd, '.git/')):
+        vcs = 'git'
+    elif os.path.isdir(os.path.join(cwd, '.hg/')):
+        vcs = 'hg'
+    elif os.path.isdir(os.path.join(cwd, '.svn/')):
+        vcs = 'svn'
+
+    if vcs == 'git':
+        log_format = ['%H', '%an', '%ae', '%aD']
+        log_format = '%x1f'.join(log_format) + '%x1e'
+        cmd_opts = ''
+
+        if options.author:
+            cmd_opts += ' --author="%s"' % options.author
+        if options.since:
+            cmd_opts += ' --since="%s"' % options.since
+
+        cmd_opts += ' %s' % (options.branch or '--all')
+        cmd_opts += ' --format="%s"' % log_format
+        cmd = 'git log' + cmd_opts
+
+    elif vcs == 'hg':
+        log_format = [
+            '{rev}:{node}',
+            '{author|person}',
+            '{author|email}',
+            '{date|rfc822date}',
+        ]
+        log_format = ';;'.join(log_format) + '\n'
+        cmd_opts = ''
+
+        if options.author:
+            cmd_opts += ' --user="%s"' % options.author
+        if options.since:
+            cmd_opts += ' --date="%s"' % options.since
+
+        cmd_opts += ' --template="%s"' % log_format
+        cmd = 'hg log' + cmd_opts
+
+    elif vcs == 'svn':
+        cmd = 'svn log --xml'
+
+    else:
+        raise Exception('Unknown VCS')
+
+    proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, cwd=cwd)
+    log, _ = proc.communicate()
+
+    if vcs == 'git':
+        log = log.decode('utf-8', 'ignore').strip('\n\x1e').split('\x1e')
+        log = [row.strip().split('\x1f') for row in log]
+    elif vcs == 'hg':
+        log = log.decode('utf-8', 'ignore').split('\n')
+        log = [row.strip().split(';;') for row in log]
+    elif vcs == 'svn':
+        from xml.dom.minidom import parseString
+        tmplog = []
+        dom = parseString(log)
+        entries = dom.getElementsByTagName('logentry')
+        for elem in entries:
+            row = [elem.getAttribute('revision')]
+            for subc in elem.childNodes:
+                if subc.nodeName == 'author':
+                    author = subc.firstChild.nodeValue
+                    row.append(author)
+                    row.append(author)
+                elif subc.nodeName == 'date':
+                    date = subc.firstChild.nodeValue
+                    dt = datetime.datetime.strptime(
+                        date, '%Y-%m-%dT%H:%M:%S.%fZ')
+                    rfcdate = formatdate(float(dt.strftime('%s')))
+                    row.append(rfcdate)
+            if len(row) == 4:
+                tmplog.append(row)
+        log = tmplog
+
+    log = [dict(list(zip(commit_fields, row))) for row in log]
+    data = {}
+
+    id_author_strategy = 'author_email'
+    if options.id_author == 'name':
+        id_author_strategy = 'author_name'
+
+    for entry in reversed(log):
+        if 'author_email' not in entry:
+            continue
+        author_id = entry[id_author_strategy]
+        if author_id not in data:
+            data[author_id] = {
+                'info': {
+                    'author_name': entry['author_name'],
+                    'author_email': entry['author_email'],
+                },
+                'streaks': {},
+                'commits': [],
+            }
+        data[author_id]['commits'].append({
+            'id': entry['id'],
+            'date': datetime.datetime.utcfromtimestamp(
+                mktime_tz(parsedate_tz(entry['date']))).date(),
+        })
+
+    for author_id in data:
+        d = data[author_id]
+
+        streaks = d['streaks']
+        streak_id = 0
+        streak_last_date = None
+
+        for entry in d['commits']:
+            if streak_last_date:
+                diff_days = (entry['date'] - streak_last_date).days
+            else:
+                diff_days = 0
+
+            if diff_days > 1:
+                if diff_days == 3 and options.exclude_weekends:
+                    day1 = entry['date'] - datetime.timedelta(days=1)
+                    day2 = entry['date'] - datetime.timedelta(days=2)
+                    if day1.weekday() == 6 and day2.weekday() == 5:  # weekend
+                        pass
+                    else:
+                        streak_id += 1
+                else:
+                    streak_id += 1
+
+            if streak_id not in streaks:
+                streaks[streak_id] = {
+                    'count': 1,
+                    'id_start': entry['id'],
+                    'id_end': entry['id'],
+                    'date_start': entry['date'],
+                    'date_end': entry['date'],
+                }
+
+            if diff_days == 0:
+                streaks[streak_id].update({
+                    'id_end': entry['id'],
+                    'date_end': entry['date'],
+                })
+            elif diff_days == 1:
+                streaks[streak_id].update({
+                    'id_end': entry['id'],
+                    'date_end': entry['date'],
+                })
+                streaks[streak_id]['count'] += 1
+
+            if diff_days >= 0:
+                streak_last_date = entry['date']
+
+    # sort streaks, setup data
+    for author_id in data:
+        d = data[author_id]
+        d['streaks'] = list(d['streaks'].values())
+        d['streaks'].sort(key=lambda x: x['count'], reverse=True)
+        d['info']['longest_streak'] = d['streaks'][0]['count']
+        d['info']['commits'] = len(d['commits'])
+        d['streaks'] = d['streaks'][:options.streaks]
+        del d['commits']
+
+    # sort users by...
+    users = list(data.values())
+    if options.sortby == 'commits':
+        users.sort(key=lambda x: x['info']['longest_streak'], reverse=True)
+        users.sort(key=lambda x: x['info']['commits'], reverse=True)
+    else:
+        users.sort(key=lambda x: x['info']['commits'], reverse=True)
+        users.sort(key=lambda x: x['info']['longest_streak'], reverse=True)
+
+    timestamp = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    for i, user in enumerate(users, 1):
+        user['info']['place'] = i
+        user['info']['timestamp'] = timestamp
+
+    if options.top:
+        users = users[:options.top]
+
+    if options.reverse:
+        users = reversed(users)
+
+    return users
+
+
+def main():
+    import sys
+    from optparse import OptionParser
+
+    if sys.version_info < (3, 0):
+        import codecs
+        sys.stdout = codecs.getwriter('utf8')(sys.stdout)
+
+    usage = 'Usage: %prog [options] REPO-PATH'
+    parser = OptionParser(usage=usage)
+
+    parser.add_option('-r', '--reverse', action='store_true', dest='reverse',
+                      default=False,
+                      help='Reverse output results')
+
+    parser.add_option('-t', '--top', type='int', dest='top', default=0,
+                      metavar='NUM',
+                      help='Show NUM top users (0 is unlimited and default)')
+
+    parser.add_option('-s', '--streaks', type='int', dest='streaks', default=5,
+                      metavar='NUM',
+                      help='Show NUM longest streaks')
+
+    parser.add_option('-e', '--exclude-weekends', action='store_true',
+                      dest='exclude_weekends',
+                      help='Exclude weekends')
+
+    parser.add_option('-n', '--name', dest='id_author', const='name',
+                      action='store_const',
+                      help='Alias for --id-author=name')
+
+    parser.add_option('--id-author', dest='id_author', default='email',
+                      metavar='ID',
+                      help='Author ID strategy [name, email]. email is default')
+
+    parser.add_option('--author', dest='author',
+                      metavar='PATTERN',
+                      help='Limit the commits output to ones with author header')
+
+    parser.add_option('--since', dest='since',
+                      metavar='DATE',
+                      help='Show commits more recent than a specific date')
+
+    parser.add_option('--branch', dest='branch', default=None,
+                      metavar='NAME',
+                      help='Branch name (all branches by default)')
+
+    parser.add_option('--sortby', dest='sortby', default='streaks',
+                      metavar='NAME',
+                      help='Sort by [streaks, commits] (streaks is default)')
+
+    parser.add_option('--output', dest='output', default='simple',
+                      metavar='FORMAT',
+                      help='Output print format [simple, json, xml, yaml]')
+
+    options, args = parser.parse_args()
+    if not args:
+        parser.error('REPO-PATH not given')
+
+    data = calculate(options, args)
+
+    if options.output == 'json':
+        import json
+        def date_handler(obj):
+            return obj.isoformat() if hasattr(obj, 'isoformat') else obj
+        print(json.dumps(list(data), default=date_handler, indent=4,
+                         sort_keys=True, ensure_ascii=False))
+
+    elif options.output == 'yaml':
+        import yaml
+        print(yaml.safe_dump(list(data), encoding='utf-8',
+                             default_flow_style=False, indent=4, width=70))
+
+    elif options.output == 'xml':
+        import dicttoxml
+        print(dicttoxml.dicttoxml(list(data)))
+
+    else:
+        for entry in data:
+            info = entry['info']
+            if options.id_author == 'name':
+                author_str = info['author_name']
+            else:
+                if info['author_name'] == info['author_email']:
+                    author_str = info['author_name']
+                else:
+                    author_str = '%s <%s>' % (info['author_name'],
+                                              info['author_email'])
+
+            print('%5d) %s (%d commits)' %
+                  (info['place'],
+                   author_str.encode('utf-8', 'ignore').decode('utf-8'),
+                   info['commits']))
+
+            for streak in entry['streaks']:
+                print('\t%4d %s: %s - %s (%s..%s)' %
+                      (streak['count'],
+                       'days' if streak['count'] > 1 else 'day ',
+                       streak['date_start'],
+                       streak['date_end'],
+                       streak['id_start'][:10],
+                       streak['id_end'][:10]))
+        if len(data):
+            print(data[0]['info']['timestamp'])
+
+
+if __name__ == '__main__':
+    main()
